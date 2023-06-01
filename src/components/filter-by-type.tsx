25a13b9f0772464c1b0a7d51f5c2e6db2f324f72
Allan Lobato <allandonizetti@gmail.com>
2023-06-01 18:11:47 -0300

    feat: add filter context

--- src/components/filter-by-type.tsx
+++ src/components/filter-by-type.tsx
@@ -5,30 +5,31 @@ export function FilterByType() {
     const { type, setType } = useContext(FilterContext);
 
     const handleChangeType = (type: string) => {
-
-        return (
-            <ul className="flex items-center justify-center gap-10">
-                <li
-                    className={`font-inherit font-normal text-base text-center uppercase text-text-dark hover:border-orange-low cursor-pointer transition-colors duration-200 ${type === 'All' ? 'border-b-4 border-orange-low font-bold' : ''
-                        }`}
-                    onClick={() => setType('All')}
-                >
-                    Todos os Produtos
-                </li>
-                <li
-                    className={`font-inherit font-normal text-base text-center uppercase text-text-dark hover:border-orange-low cursor-pointer transition-colors duration-200 ${type === 'Camisetas' ? 'border-b-4 border-orange-low font-bold' : ''
-                        }`}
-                    onClick={() => setType('Camisetas')}
-                >
-                    Camisetas
-                </li>
-                <li
-                    className={`font-inherit font-normal text-base text-center uppercase text-text-dark hover:border-orange-low cursor-pointer transition-colors duration-200 ${type === 'Canecas' ? 'border-b-4 border-orange-low font-bold' : ''
-                        }`}
-                    onClick={() => setType('Canecas')}
-                >
-                    Canecas
-                </li>
-            </ul>
-        );
     }
+
+    return (
+        <ul className="flex items-center justify-center gap-10">
+            <li
+                className={`font-inherit font-normal text-base text-center uppercase text-text-dark hover:border-orange-low cursor-pointer transition-colors duration-200 ${type === 'All' ? 'border-b-4 border-orange-low font-bold' : ''
+                    }`}
+                onClick={() => setType('All')}
+            >
+                Todos os Produtos
+            </li>
+            <li
+                className={`font-inherit font-normal text-base text-center uppercase text-text-dark hover:border-orange-low cursor-pointer transition-colors duration-200 ${type === 'Camisetas' ? 'border-b-4 border-orange-low font-bold' : ''
+                    }`}
+                onClick={() => setType('Camisetas')}
+            >
+                Camisetas
+            </li>
+            <li
+                className={`font-inherit font-normal text-base text-center uppercase text-text-dark hover:border-orange-low cursor-pointer transition-colors duration-200 ${type === 'Canecas' ? 'border-b-4 border-orange-low font-bold' : ''
+                    }`}
+                onClick={() => setType('Canecas')}
+            >
+                Canecas
+            </li>
+        </ul>
+    );
+}
